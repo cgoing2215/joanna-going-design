e460db435a0557a36580ba33fe6bcb18358d1b7a
Caroline Going <carolinegoing2@gmail.com>
2023-07-06 12:04:03 -0400

    updated projects, about and contact pages

--- js/script.js
+++ js/script.js
@@ -1,14 +1,92 @@
-const navOptions = document.querySelectorAll('.nav-option');
+// Allows fade in animation for content on page
+const elements = document.querySelectorAll('.fade-in');
 
-function expandOption() {
-  this.style.transform = 'scale(1.1)';
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.15 });
+
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+
+
+// Opens image upon click
+const images = document.getElementsByClassName('image');
+const modal = document.getElementById('image-modal');
+const modalImage = document.getElementById('modal-image');
+const photos = document.querySelectorAll('.photo-section img')
+let currentImageIndex = 0;
+
+function openImage(image) {
+  modal.style.display = 'block';
+  modalImage.src = image.src;
+  currentIndex = Array.from(images).indexOf(image);
 }
 
-function shrinkOption() {
-  this.style.transform = 'scale(1)';
+modalImage.onload = function() {
+  var modalWidth = modal.offsetWidth - 40; // Adjust for padding and borders
+  var modalHeight = modal.offsetHeight - 40;
+  var imageWidth = modalImage.naturalWidth;
+  var imageHeight = modalImage.naturalHeight;
+
+  if (imageWidth > modalWidth) {
+    var widthRatio = modalWidth / imageWidth;
+    imageWidth *= widthRatio;
+    imageHeight *= widthRatio;
+  }
+
+  if (imageHeight > modalHeight) {
+    var heightRatio = modalHeight / imageHeight;
+    imageWidth *= heightRatio;
+    imageHeight *= heightRatio;
+  }
+
+  modalImage.style.width = imageWidth + 'px';
+  modalImage.style.height = imageHeight + 'px';
+};
+
+function closeImage() {
+  modal.style.display = 'none';
 }
 
-navOptions.forEach(option => {
-  option.addEventListener('mouseover', expandOption);
-  option.addEventListener('mouseout', shrinkOption);
+// Navigate through gallery of photos using arrows
+
+function navigateForward() {
+  currentImageIndex++;
+  if (currentImageIndex >= photos.length){
+    currentImageIndex = 0; // wrap around to the first image
+  }
+  openImage(photos[currentImageIndex]);
+}
+
+function navigateBackward() {
+  currentImageIndex--;
+  if (currentImageIndex <0){
+    currentImageIndex = photos.length - 1;
+  }
+  openImage(photos[currentImageIndex]);
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 37) {
+    navigateBackward()
+  } else if (event.keyCode === 39) {
+    navigateForward();
+  }
+}
+
+// Event listeners for previous and next buttons
+document.getElementById('previous-btn').addEventListener('click', function() {
+  navigateBackward();
 });
+
+document.getElementById('next-btn').addEventListener('click', function() {
+  navigateForward();
+});
+
+document.addEventListener('keydown', handleKeyPress);
